9b023a445b292cc77cb8a0c1e63da7d6ea4d1a03
galihchuukichi <134302485+galihchuukichi@users.noreply.github.com>
2024-03-04 00:23:25 +0800

    Update class.cpp

--- class.cpp
+++ class.cpp
@@ -36,6 +36,40 @@ int main(){
 
 
 // USING CONSTRUCTOR
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Mahasiswa{
+//     public:
+//         string nama;
+//         string cryptoAset;
+//         double banyakAset;
+// };
+
+// int main(){
+//     Mahasiswa data1, data2;
+//     data1.nama = "Galih";
+//     data1.cryptoAset = "ONDO";
+//     data1.banyakAset = 40.1;
+
+//     data2.nama = "Bintang";
+//     data2.cryptoAset = "MAVIA";
+//     data2.banyakAset = 10.0;
+    
+//     cout << data1.nama << endl;
+//     cout << data1.cryptoAset << endl;
+//     cout << data1.banyakAset << endl;
+    
+    
+//     cout << data2.nama << endl;
+//     cout << data2.cryptoAset << endl;
+//     cout << data2.banyakAset << endl;
+
+
+//     return 0;
+// }
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -46,25 +80,22 @@ class Aset{
         double amountInUSD;
         
         // constructor dengan parameter
-        Aset(string inputJenis, string inputNama, double inputAmountInUSD){
+        Aset(string inputNamaVariabel, string inputJenis, string inputNama, double inputAmountInUSD){
             Aset::jenis = inputJenis;
             Aset::namaAset = inputNama;
             Aset::amountInUSD = inputAmountInUSD;
+            cout<<"Pemilik: "<<inputNamaVariabel<<endl;
+            cout<<Aset::jenis<<endl;
+            cout<<Aset::namaAset<<endl;
+            cout<<Aset::amountInUSD<<endl;
         }
 
 };
 
 
 int main(){
-    Aset galih = Aset("crypto","ONDO",25.0);
-    cout<<galih.jenis<<endl;
-    cout<<galih.namaAset<<endl;
-    cout<<galih.amountInUSD<<endl;
-    
-    Aset huehue = Aset("property","KOST",1000.0);
-    cout<<huehue.jenis<<endl;
-    cout<<huehue.namaAset<<endl;
-    cout<<huehue.amountInUSD<<endl;
+    Aset galih = Aset("galih","crypto","ONDO",25.0);
+    Aset huehue = Aset("huehue","property","KOST",1000.0);
 
 
     cout<<"PROGRAM KELAR"<<endl;
